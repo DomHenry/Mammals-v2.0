c0ea09fccb003ed42a97d746df7bd3931b1024ff
Dominic Henry <dominichenry@gmail.com>
2021-12-06 10:30:27 -0500

    Added step layout

--- src/00_user run.R
+++ src/00_user run.R
@@ -1,15 +1,4 @@
-## ________________________________________________________________________
-
-## Title:
-## Purpose:
-## Author:
-## Date:
-
-## Libraries
-
-## ________________________________________________________________________
-
-## Define species
+# Step 1: select focal species --------------------------------------------
 
 # sppselect <- "Amblysomus corriae"
 # sppselect <- "Amblysomus septentrionalis"
@@ -45,35 +34,43 @@
 # sppselect <- "Scotophilus nigrita "
 # sppselect <- "Taphozous perforatus"
 
+
+# Step 2: extract the occurrence data from EWT database -------------------
 source("src/01_extract and process occ data.R")
 rm(list = ls()[!(ls() %in% "sppselect")])
 
+# Step 3: create plots ----------------------------------------------------
 source("src/02_spatiotemporal occ plots.R")
 rm(list = ls()[!(ls() %in% "sppselect")])
 
+# Step 4: prepare data for SDMs  ------------------------------------------
 source("src/03_prepare SDM inputs.R")
 rm(list = ls()[!(ls() %in% "sppselect")])
 
+# Step 5: Run BART SDMs ---------------------------------------------------
 source("src/04_BART SDMs.R")
 rm(list = ls()[!(ls() %in% "sppselect")])
 
 
-# Loop to process bats ----------------------------------------------------
+
+# Not run -----------------------------------------------------------------
+
+# Loop to process bats
 bat_list <- c("Cistugo seabrae", "Kerivoula argentata", "Miniopterus inflatus", "Nycteris woodi",
               "Otomops martiensseni", "Rhinolophus blasii", "Rhinolophus denti", "Rhinolophus smithersi",
               "Scotoecus albofuscus", "Scotophilus nigrita", "Taphozous perforatus")
 
 
 for(i in 10:length(bat_list)){
-  
+
   sppselect <- bat_list[[i]]
-  
+
   source("src/01_extract and process occ data.R")
   rm(list = ls()[!(ls() %in% c("sppselect","bat_list"))])
-  
+
   source("src/02_spatiotemporal occ plots.R")
   rm(list = ls()[!(ls() %in% c("sppselect","bat_list"))])
-  
+
   print("COMPLETED")
 
 }
