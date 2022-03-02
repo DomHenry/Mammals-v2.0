dac6e9951a5a3bacbf46af38e96254959b0d01bf
Dominic Henry <dominichenry@gmail.com>
2022-03-02 10:10:56 -0500

    Updated to handle IUCN range outside of RSA

--- helper functions/check_occ_QDS_IUCN.R
+++ helper functions/check_occ_QDS_IUCN.R
@@ -1,15 +1,18 @@
 # Check whether points fall within QDS/IUCN polygons -------------------------
 check_occ_QDS_IUCN <- function(x, occ) {
-  
+
+
+  if(nrow(x) > 0){
+
   # x <- st_union(x)
   # x <- sf::st_make_valid(x)
   x <- as(x, "Spatial")
   buff1 <- raster::buffer(x, width = 0.05)
   buff2 <- raster::buffer(x, width = 0.1)
   buff3 <- raster::buffer(x, width = 0.2)
-  
+
   # rgeos::gIsValid(x, byid = FALSE, reason=TRUE)
-  
+
   b1 <- buff1 - x
   b2 <- buff2 - buff1
   b3 <- buff3 - buff2
@@ -25,6 +28,17 @@ check_occ_QDS_IUCN <- function(x, occ) {
       mutate(buff1 = st_intersects(occ, st_as_sf(b1), sparse = FALSE)) %>%
       mutate(buff2 = st_intersects(occ, st_as_sf(b2), sparse = FALSE)) %>%
       mutate(buff3 = st_intersects(occ, st_as_sf(b3), sparse = FALSE))
+  }
+  }
+
+  else{
+
+    occ <- occ %>%
+      mutate(core = FALSE) %>%
+      mutate(buff1 = FALSE) %>%
+      mutate(buff2 = FALSE) %>%
+      mutate(buff3 = FALSE)
+
   }
 
   return(occ)
